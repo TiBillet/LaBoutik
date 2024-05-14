25963d4509cbfd436852c6b48a8c8ecea4765e79
filaos974 <filaos974@hotmail.com>
2024-05-14 13:12:48 +0400

    delete favicon.ico

--- flush.sh
+++ flush.sh
@@ -5,7 +5,7 @@ if [ "$TEST" = "1" ]
 then
     poetry run python manage.py migrate
     poetry run python manage.py flush --no-input
-#    poetry run python manage.py collectstatic --no-input
+    poetry run python manage.py collectstatic --no-input
     poetry run python manage.py install --tdd
     poetry run python manage.py runserver 0.0.0.0:8000
 else
