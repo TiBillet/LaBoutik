bb4058a23183ad47e76314a87fdbd34fd9ecc808
Kay Navarro <pro@kayathommy.fr>
2024-09-04 21:22:29 +0200

    shabang header fix ("exec format error") and leaner manage.py calls

--- flush.sh
+++ flush.sh
@@ -1,13 +1,13 @@
-# /bin/bash
+#! /bin/bash
 set -e
 
 if [ "$TEST" = "1" ]
 then
-    poetry run python manage.py migrate
-    poetry run python manage.py flush --no-input
-#    poetry run python manage.py collectstatic --no-input
-    poetry run python manage.py install --tdd
-    poetry run python manage.py runserver 0.0.0.0:8000
+    poetry run ./manage.py migrate
+    poetry run ./manage.py flush --no-input
+#    poetry run ./manage.py collectstatic --no-input
+    poetry run ./manage.py install --tdd
+    poetry run ./manage.py runserver 0.0.0.0:8000
 else
     echo "TEST environment variable is not set"
 fi
