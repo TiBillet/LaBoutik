956ec478bf5ef31538f31885645c247f3a6dcb08
Jonas Legion <jturbeaux@pm.me>
2024-09-22 10:55:40 +0200

    collectstatic in flush

--- flush.sh
+++ flush.sh
@@ -5,8 +5,8 @@ if [ "$TEST" = "1" ]
 then
     poetry run ./manage.py flush --no-input
     poetry run ./manage.py migrate
-#    poetry run ./manage.py collectstatic --no-input
     poetry run ./manage.py install --tdd
+    poetry run ./manage.py collectstatic --no-input
     poetry run ./manage.py runserver 0.0.0.0:8000
 else
     echo "TEST environment variable is not set"
