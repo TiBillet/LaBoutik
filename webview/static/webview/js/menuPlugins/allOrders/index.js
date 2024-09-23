747e6e06ceb044f344a6ec68dde6c956ad0d0721
filaos974 <filaos974@hotmail.com>
2024-09-23 14:33:39 +0400

    ajout bt allOrders

---
+++ webview/static/webview/js/menuPlugins/allOrders/index.js
@@ -0,0 +1,9 @@
+window.allOrderInterface = function () {
+// TODO
+}
+
+export const menu = {
+  func: "allOrderInterface",
+  icon: "fas fa-cash-register", // font awesome 5
+  i8nIndex: "allOrders,uppercase"
+}
