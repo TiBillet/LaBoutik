cb7dda6f10fbfafce05e408e4f8cf6cb9ec55b89
filaos974 <filaos974@hotmail.com>
2024-10-08 19:43:11 +0400

    ajout bouton all orders

--- webview/static/webview/js/menuPlugins/allOrders/index.js
+++ webview/static/webview/js/menuPlugins/allOrders/index.js
@@ -1,9 +1,17 @@
 window.allOrderInterface = function () {
-// TODO
+  // hide menu
+  document.querySelector('#menu-burger-conteneur').classList.toggle('burger-show')
+
+  // bascule l'affichage et l'éffacement de pages
+  sys.effacerElements(['#page-commandes', '#tables', '#commandes-table'])
+  sys.afficherElements(['#service-commandes,block'])
+
+  // <button hx-get="allOrders/null" hx-target="#service-commandes">clique</button>
+  htmx.ajax('GET', 'allOrders/null', '#service-commandes')
 }
 
 export const menu = {
   func: "allOrderInterface",
-  icon: "fas fa-cash-register", // font awesome 5
+  icon: "fas fa-concierge-bell", // font awesome 5
   i8nIndex: "allOrders,uppercase"
 }
