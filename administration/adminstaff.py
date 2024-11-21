bed746f236184c323ab19b5d2690834b55d7a6b2
JonasFW13 <jturbeaux@pm.me>
2024-11-21 09:39:18 +0100

    migration

--- administration/adminstaff.py
+++ administration/adminstaff.py
@@ -838,10 +838,11 @@ class ConfigurationAdmin(SingletonModelAdmin):
         ('Options', {
             'fields': (
                 # ('appareillement', 'pin_code_primary_link',),
+                'void_card',
                 'validation_service_ecran',
                 'remboursement_auto_annulation',
                 # 'domaine_cashless',
-                'ip_cashless',
+                # 'ip_cashless',
             ),
         }),
         ('Adhésion', {
