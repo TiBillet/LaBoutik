b390f0f8a4dc9cb267dca9c634faa22687390d06
JonasFW13 <jturbeaux@pm.me>
2025-09-27 12:54:31 +0200

    Update `env_test2` values and enhance `import_articles` command:
    - Modify `LESPASS_TENANT_URL`, `MAIN_ASSET_NAME`, and `DEMO_TAGID_CM` in `env_test2`.
    - Adjust CSV import logic in `import_articles`: reorder fields, add `tva` handling, and update relationships.

--- administration/management/commands/import_articles.py
+++ administration/management/commands/import_articles.py
@@ -3,13 +3,13 @@ from decimal import Decimal
 
 from django.core.management.base import BaseCommand
 
-from APIcashless.models import dround, Categorie, ArticleVendu, Articles, PointDeVente
+from APIcashless.models import dround, Categorie, ArticleVendu, Articles, PointDeVente, TauxTVA
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         print(
-            "CSV format: categorie, tva, titre, prix")
+            "CSV format: titre, categorie, prix, tva")
         # file = open('data/retour_usine_raff_gen_2.csv')
         input_fichier_csv = input('path fichier csv ? \n')
         csv_file = open(input_fichier_csv)
@@ -19,24 +19,30 @@ class Command(BaseCommand):
         list_articles = []
         for line in csv_parser:
             print(line)
-            categorie = line[0].capitalize()
-            if not categorie or not type(categorie) == str :
-                raise Exception('categorie')
 
-            titre = line[1].capitalize()[:30]
+            titre = line[0].capitalize()[:30]
             if not type(titre) == str or not titre:
                 raise Exception('titre')
+
+            categorie = line[1].capitalize()
+            if not categorie or not type(categorie) == str :
+                raise Exception('categorie')
 
             tarif = Decimal(line[2].replace(',','.')).quantize(Decimal('1.00'))
             if not type(tarif) == Decimal or not tarif:
                 raise Exception('tarif')
 
+            tva = Decimal(Decimal(line[3]) *100).quantize(Decimal('1.00'))
+            if not type(tva) == Decimal or not tva:
+                raise Exception('Tva')
+
             print(f"categorie: {categorie}, titre: {titre}, tarif: {tarif}")
-            list_articles.append((categorie, titre, tarif))
+            list_articles.append((titre, categorie, tarif, tva))
         csv_file.close()
 
-        boutique = PointDeVente.objects.get(name='Boutique')
+        pdv = PointDeVente.objects.get(name='RESTO')
         for article in list_articles:
-            cat, created = Categorie.objects.get_or_create(name=article[0])
+            tva, created = TauxTVA.objects.get_or_create(taux=article[3], name=f"{article[3]}%")
+            cat, created = Categorie.objects.get_or_create(name=article[1], tva=tva)
             art, created = Articles.objects.get_or_create(categorie=cat, name=article[1], prix=article[2])
-            boutique.articles.add(art)
+            pdv.articles.add(art)
